6a905b51f95561d9887f17adc132dc2141ff1540
DonMB77 <purposefuldrifter@gmail.com>
2025-03-08 12:24:57 +0100

    UserInput

--- Output.cs
+++ Output.cs
@@ -75,4 +75,20 @@ namespace cSharpSB
             Console.ReadKey();
         }
     }
+
+    class UserInput
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+
+            Console.ReadKey();
+        }
+    }
 }
