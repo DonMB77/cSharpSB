a90034823c8da05c61e5b66e3e609ba541fe911d
DonMB77 <purposefuldrifter@gmail.com>
2025-03-08 10:58:34 +0100

    Output.cs

---
+++ Output.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpSB
+{
+    class Output
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Hey");
+            Console.WriteLine("Hello");
+
+            //This is comment
+            /*
+             * This
+             * is
+             * a
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("\tDon");
+
+            Console.ReadKey();
+        }
+    }
+}
--- obj/Debug/net8.0/cSharpSB.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpSB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpSB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36012ebeea591aa1872848d0e15b828aa85372ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpSB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpSB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
