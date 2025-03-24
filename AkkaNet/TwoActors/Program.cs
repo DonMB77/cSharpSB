e424a3ed61a8937291ec7c61643aefeb98578942
DonMB77 <purposefuldrifter@gmail.com>
2025-03-24 10:37:17 +0100

    SimpleActor + TwoActors

--- AkkaNet/TwoActors/Program.cs
+++ AkkaNet/TwoActors/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cSharpSB.AkkaNet
+namespace cSharpSB.AkkaNet.TwoActors
 {
-    class SimpleActor
+    class Program
     {
     }
 }
