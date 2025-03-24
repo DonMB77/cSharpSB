7279124c28d9c1dc68a2bfe82d1f42f8967d17ce
DonMB77 <purposefuldrifter@gmail.com>
2025-03-24 10:57:49 +0100

    TransactionTypeEnum + Transaction + ConsoleUtil

---
+++ AkkaNet/TwoActors/Domain/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using cSharpSB.AkkaNet.TwoActors.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpSB.AkkaNet.TwoActors.Domain
+{
+    public class Transaction
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreateDate { get; private set; }
+        public TransactionTypeEnum TransactionTypeEnum { get; private set; }
+        public double Amount { get; private set; }
+        public String AffiliationCode { get; private set; }
+
+        public Transaction(TransactionTypeEnum transactionTypeEnum, double amount, string affiliationCode)
+        {
+            Id = Guid.NewGuid();
+            CreateDate = DateTime.UtcNow;
+            TransactionTypeEnum = transactionTypeEnum;
+            Amount = amount;
+            AffiliationCode = affiliationCode;
+        }
+    }
+}
---
+++ AkkaNet/TwoActors/Enums/TransactionTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpSB.AkkaNet.TwoActors.Enums
+{
+    public enum TransactionTypeEnum
+    {
+        ECommerce = 0,
+        TEF = 1,
+        POS = 2
+    }
+}
