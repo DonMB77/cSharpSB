17c7611f2e5bddd7f6060f11cd0d08409f28bed9
DonMB77 <purposefuldrifter@gmail.com>
2025-04-06 18:19:32 +0200

    m. A.

--- old/Program.cs
+++ old/Program.cs
@@ -7,6 +7,15 @@ namespace cSharpSB.old
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            int[,] numbers = { { 1, 2, 3 },{ 1, 2, 3} };
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.WriteLine(numbers[i,j]);
+                }
+            }
         }
     }
 }
