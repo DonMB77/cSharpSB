ac34dd9a52ba20746592e16114a7ade5aa7b9f95
DonMB77 <purposefuldrifter@gmail.com>
2025-05-03 19:05:50 +0200

    basic demo

--- AkkaNet/AkkaNetBook/AkkaNetBook/Program.cs
+++ AkkaNet/AkkaNetBook/AkkaNetBook/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+using AkkaNetBook;
+
+namespace EntryMain
+{
+    class MainClass
+    {
+        private static int Main()
+        {
+            var actorSystem = ActorSystem.Create("MyActorSystem");
+            var actorRef = actorSystem.ActorOf<PersonActor>("personActorA");
+            var adress = actorSystem.ActorSelection("/user/personActorA");
+            actorRef.Tell(new VocalGreeting("Hi"));
+            Console.WriteLine("test");
+            return 0;
+        }
+    }
+}
