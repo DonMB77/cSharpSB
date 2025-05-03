e2c1238cc54c4fb426b3ffb4d23fc3408cdf2a3f
DonMB77 <purposefuldrifter@gmail.com>
2025-05-03 13:54:06 +0200

    PersonActor : UntypedActor

--- AkkaNet/AkkaNetBook/AkkaNetBook/PersonActor.cs
+++ AkkaNet/AkkaNetBook/AkkaNetBook/PersonActor.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
 
 namespace AkkaNetBook
 {
-    class PersonActor
+    class PersonActor : UntypedActor
     {
+        protected override void OnReceive(object message)
+        {
+            if (message is VocalGreeting)
+            {
+                message.
+            }
+        }
+    }
+
+    class Wave { }
+
+    class VocalGreeting
+    {
+        private readonly string _greeting;
+
+        public string Greeting { get { return _greeting; } }
+
+        public VocalGreeting(string greeting)
+        {
+            _greeting = greeting;
+        }
     }
 }
